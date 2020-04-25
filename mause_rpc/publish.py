112ea9f7d69b236f486e2c24039e1ddecefe3cd1
Dominic May <me@mause.me>
2020-04-25 16:47:49 +0800

    Automate bump

--- mause_rpc/publish.py
+++ mause_rpc/publish.py
@@ -1,9 +1,19 @@
 from pathlib import Path
+from subprocess import check_call
 
 from poetry_publish.publish import poetry_publish
+from semver import parse_version_info
 
 import mause_rpc
 
+version = parse_version_info(mause_rpc.__version__).bump_patch()
+
+with open('mause_rpc/__init__.py', 'w') as fh:
+    fh.write(f'__version__ = \'{version}\'\n')
+
+check_call(['poetry', 'version', str(version)])
+check_call(['git', 'add', 'pyproject.toml', 'mause_rpc/__init__.py'])
+check_call(['git', 'commit', '-m', f'Bump to {version}'])
 
 poetry_publish(
     package_root=Path(mause_rpc.__file__).parent.parent,
